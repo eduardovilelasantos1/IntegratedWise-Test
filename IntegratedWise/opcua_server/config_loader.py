72b1bc9ad83978565c280acadb58d3330ab084cc
eduardovilelasantos1 <eduardovilelasantos1@gmail.com>
2025-12-15 08:17:23 -0300

    Delete IntegratedWise directory

--- IntegratedWise/opcua_server/config_loader.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-CONFIG_DIR = os.path.join(os.path.dirname(__file__), '..', 'configs')
-
-def load_opcua_config():
-    with open(os.path.join(CONFIG_DIR, 'config_opcua.json'), 'r') as f:
-        return json.load(f)
